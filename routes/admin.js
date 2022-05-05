d90688724096b97743fed58329b91a34f22db00a
Olugbenga Ajayi <gbejula@gmail.com>
2022-05-05 21:48:29 +0100

    made correction to ejs files and added controllers

--- routes/admin.js
+++ routes/admin.js
@@ -2,22 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  //console.log('In another middleware');
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+router.post('/add-product', productsController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
